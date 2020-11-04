ad1eaead9c4791d548011fe7af69e0da34032965
Natalia Pavlova <natalja1912@yandex.ru>
2020-11-04 23:06:31 +0300

    feat: sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,14 @@
+//кнопка для открытия/скрытия сайдбара
+const button = document.getElementById('hamburger-menu'),
+    span = button.getElementsByTagName('span')[0];
 
+button.onclick = function () {
+    span.classList.toggle('hamburger-menu-button-close');
+};
 
+//открытие/скрытие сайдбара
+const sidebarToggle = document.querySelector('.hamburger-menu-button');
+const sidebar = document.querySelector('.sidebar');
+sidebarToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar_active');
+} )
