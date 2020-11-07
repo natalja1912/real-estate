f0fdd650fc46a9ff4de6500d4834b4cd662417d7
Natalia Pavlova <natalja1912@yandex.ru>
2020-11-07 20:05:35 +0300

    feat: polyfill

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+/**
+ * NodeList.prototype.forEach() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
+ */
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
+
 /* кнопка для открытия/скрытия сайдбара */
 const button = document.getElementById('hamburger-menu'),
     span = button.getElementsByTagName('span')[0];
